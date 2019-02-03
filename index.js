74cd125650586f826758307a407a019ae22e97c0
Nicholas Peretti <peretti.nicholas@gmail.com>
2019-02-03 18:51:34 +0100

    Add webpack-dev-middleware

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackConfig = require('./config/webpack')
 const { port, publicDirectory } = require('./config/express')
+
+const compiler = webpack(webpackConfig)
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath || '/'
+  })
+)
 
 app.use(
   express.static(publicDirectory)
