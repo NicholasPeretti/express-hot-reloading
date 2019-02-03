d50e51e15ac190ca9a61596f6d58455a749ece2b
Nicholas Peretti <peretti.nicholas@gmail.com>
2019-02-03 19:09:01 +0100

    Add webpack-hot-middleware to the express server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
 const webpackConfig = require('./config/webpack')
 const { port, publicDirectory } = require('./config/express')
 
@@ -10,6 +11,10 @@ app.use(
   webpackDevMiddleware(compiler, {
     publicPath: webpackConfig.output.publicPath || '/'
   })
+)
+
+app.use(
+  webpackHotMiddleware(compiler)
 )
 
 app.use(
