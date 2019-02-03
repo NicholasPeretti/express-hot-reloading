fee647072341908e29d80e35de03edeae0a4d817
Nicholas Peretti <peretti.nicholas@gmail.com>
2019-02-03 19:08:40 +0100

    Update webpack configuration

--- config/webpack.js
+++ config/webpack.js
@@ -1,13 +1,18 @@
+const webpack = require('webpack')
 const path = require('path')
 const { publicDirectory } = require('./express')
 
 module.exports = {
   mode: 'development',
   entry: [
+    'webpack-hot-middleware/client',
     path.resolve(__dirname, '../src')
   ],
   output: {
     path: publicDirectory,
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
